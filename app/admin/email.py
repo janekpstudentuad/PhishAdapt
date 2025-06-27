9c1464ade8b9a3f99da9787ac9bbfb921aece35c
janekocanpayne <39746672+janekocanpayne@users.noreply.github.com>
2025-06-27 07:03:20 +0100

    Add personalisation to email

--- app/admin/email.py
+++ app/admin/email.py
@@ -1,6 +1,7 @@
 from flask import render_template, current_app
 from app.email import send_email
 from app.utils.jwt_tokens import get_training_token
+from app.models import Profile
 
 def send_voicemail(user, campaign):
     token = get_training_token(user.id, campaign.id)
@@ -10,7 +11,8 @@ def send_voicemail(user, campaign):
                html_body=render_template('email/baseline_voicemail.html', user=user, token=token))
 
 def send_training_invitation(user):
+    profile = Profile.query.filter_by(user_id=user.id).first()
     send_email(f'"Training recommendations for {user.username}',
     sender='voicemail@company.com',
     recipients=[user.email],
-    html_body=render_template('email/training_invite.html', user=user))
+    html_body=render_template('email/training_invite.html', user=user, profile=profile))
